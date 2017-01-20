9943084ab9dcd87d252a9badaf98f59d914e2993
slolobdill44 <slolobdill44@gmail.com>
2017-01-20 01:45:14 -0800

    Paperclip fix for region

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module HamcampsApp
     # config.i18n.default_locale = :de
     config.paperclip_defaults = {
       :storage => :s3,
+      :s3_region => ENV["s3_region"],
       s3_host_name: 's3-us-west-1.amazonaws.com',
       :s3_credentials => {
         :bucket => ENV["s3_bucket"],
