4e52365521caec9a6f5f1261b3b6f015d23d16c7
Adrian Lobdill <slolobdill44@gmail.com>
2017-04-02 18:46:06 -0700

    Adds support for LaunchDarkly header changes and fixes logout issue

--- frontend/components/main_header/main_header.jsx
+++ frontend/components/main_header/main_header.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router';
 class MainHeader extends React.Component {
 
   render () {
-    const logoLink = `/artists/${this.props.user.id}`;
+    const logoLink = this.props.user === null ? '/' : `/artists/${this.props.user.id}`;
 
     return (
       <div className="main-header">
--- frontend/components/splash_header/splash_header.jsx
+++ frontend/components/splash_header/splash_header.jsx
@@ -2,12 +2,40 @@ import React from 'react';
 import AccountInfo from './account_info';
 import SearchBarContainer from './search_bar/search_bar_container';
 import { Link, hashHistory } from 'react-router';
+import ldClient from 'ldclient-js';
 
 class SplashHeader extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      headerBackgroundColor: "#E6EBE0"
+    }
+  }
+
+  componentDidMount() {
+    console.log(this.props);
+
+    let user = {
+        "key": "aa0ceb",
+        "firstName": "Ernestina"
+    };
+
+    let ldclient = ldClient.initialize('58e1714291c11e0962c37215', user);
+
+    const that = this;
+
+    ldclient.on('ready', function() {
+      console.log("It's now safe to request feature flags");
+      let showFeature = ldclient.variation("header-color");
+      that.setState({headerBackgroundColor: `${showFeature}`});
+    });
+  }
 
   render () {
+
     return (
-      <div className="splash-header">
+      <div style={{backgroundColor: this.state.headerBackgroundColor}}  className="splash-header">
         <section className="splash-left">
           <img className="splash-logo" src="http://res.cloudinary.com/adrianlobdill/image/upload/c_scale,w_100/v1485153364/hamcamp_logo_200px_V2_jeifyy.png" />
           <div className="splash-left-text">
