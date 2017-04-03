c538885039ade9fd4b1af05374c6aacdc8e79bb2
Adrian Lobdill <slolobdill44@gmail.com>
2017-04-02 18:55:57 -0700

    Adds support for LaunchDarkly changes to splash and main headers

--- frontend/components/main_header/main_header.jsx
+++ frontend/components/main_header/main_header.jsx
@@ -2,14 +2,45 @@ import React from 'react';
 import AccountInfo from '../splash_header/account_info';
 import SearchBarContainer from '../splash_header/search_bar/search_bar_container';
 import { Link } from 'react-router';
+import ldClient from 'ldclient-js';
 
 class MainHeader extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      headerBackgroundColor: "#E6EBE0"
+    }
+  }
+
+  componentDidMount() {
+    // console.log(this.props);
+
+    let user = {
+        "key": "aa0ceb",
+        "firstName": this.props.user.username
+    };
+    console.log(user);
+
+    let ldclient = ldClient.initialize('58e1714291c11e0962c37215', user);
+
+    const that = this;
+
+    ldclient.on('ready', function() {
+      console.log("It's now safe to request feature flags");
+      let showFeature = ldclient.variation("header-color");
+      console.log(showFeature);
+      that.setState({headerBackgroundColor: `${showFeature}`});
+    });
+  }
+
 
   render () {
+    // console.log(this.props.user);
     const logoLink = this.props.user === null ? '/' : `/artists/${this.props.user.id}`;
 
     return (
-      <div className="main-header">
+      <div style={{backgroundColor: this.state.headerBackgroundColor}} className="main-header">
         <div className="main-nav-left">
           <Link to={logoLink}>
             <img className="main-logo" src="http://res.cloudinary.com/adrianlobdill/image/upload/c_scale,h_37/v1485154475/hamcamp_logo_100px_V2_drto1u.png" />
